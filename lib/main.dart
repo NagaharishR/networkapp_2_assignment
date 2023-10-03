82fc6675f25694c701514c58063b1eeadf8d996b
New <rnagaharish@gmail.com>
2023-10-03 12:24:27 +0530

    String

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Album {
   final int id;
   final String title;
   Album({required this.userId, required this.id, required this.title});
-  factory Album.fromJson(Map<dynamic, dynamic> json){
+  factory Album.fromJson(Map<String, dynamic> json){
     return Album(userId: json['userId'], id: json['id'], title: json['title']);
 
   }
